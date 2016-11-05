4c434a5c6c596f18cba7c150a71d8cf7057d83d7
Peter Muys <peter@petermuys.com>
2016-11-05 09:01:25 +0100

    wip sql maven plugin import db

---
+++ src/main/java/com/persistentbit/sql/mavenplugin/SqlImportDbMojo.java
@@ -0,0 +1,48 @@
+package com.persistentbit.sql.mavenplugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import java.io.File;
+
+/**
+ * create a Db substema by importing the data structure from an existing database.
+ *
+ * @author petermuys
+ * @since 5/11/16
+ */
+@Mojo(
+	name="generate-db",
+	defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+	requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME
+)
+public class SqlImportDbMojo extends AbstractMojo{
+
+	@Parameter(defaultValue = "src/main/resources",required = true)
+	File resourcesDirectory;
+
+	@Parameter(required = true)
+	String dbDriverClass;
+	@Parameter(required = true)
+	String dbUrl;
+	@Parameter
+	String userName;
+	@Parameter
+	String password;
+
+	public void execute()  throws MojoExecutionException, MojoFailureException {
+		try{
+			getLog().info("Creating database connection");
+
+		}catch (Exception e){
+			getLog().error("General error",e);
+			throw new MojoFailureException("Error while generating db code",e);
+		}
+
+	}
+}
